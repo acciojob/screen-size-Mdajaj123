34245f073b48ef1d3e952f08b96fca9e6ff18e38
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-11-01 19:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.addEventListener("resize" ,()=>{
+	let width=window.innerWidth;
+	let height=window.innerHeight;
+	let div=document.getElementById("sizeInfo");
+	div.innerHTML=`<h1>width: ${width} and Height: ${height}</h1>`
+})
